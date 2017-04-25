f66f955eb4eb0e77196d2a37da35c7072b79727e
David Gaudreault <david.gaudreault@gmail.com>
2017-04-25 06:45:59 -0400

    creating, listing, updating and retrieving articles done

--- conduit/apps/profiles/models.py
+++ conduit/apps/profiles/models.py
@@ -23,5 +23,36 @@ class Profile(TimestampedModel):
 
     image = models.URLField(blank=True)
 
+    follows = models.ManyToManyField(
+        'self',
+        related_name='followed_by',
+        symmetrical=False
+    )
+
     def __str__(self):
         return self.user.username
+
+    def follow(self, profile):
+        """Follow profile if we're not already following profile."""
+        self.follows.add(profile)
+
+    def unfollow(self, profile):
+        """Unfollow profile if we're already following profile"""
+        self.follows.remove(profile)
+
+    def is_following(self, profile):
+        """Returns True is we're following profile; False otherwise"""
+        return self.follows.filter(pk=profile.pk).exists()
+
+    def is_followed_by(self, profile):
+        """Return True if profile is following us; False otherwise."""
+        return self.followed_by.filter(pk=profile.pk).exists()
+
+    # This is an example of a Many-To-Many relationship where both sides of the
+    # relationship are of the same model. In this case, the model is `Profile`.
+    # As mentioned in the text, this relationship will be one-way. Just because
+    # you are following mean does not mean that I am following you. This is
+    # what `symmetrical=False` does for us.
+
+
+
